8716142d73e9e4b116d689f5db4299efee106e78
T4R_da <surge4335@gmail.com>
2026-04-19 15:01:13 +0200

    directory minor change

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,7 @@ void printMenu(const std::array<std::string, 4>& options, int selected) {
 
 int main() {
     std::string FILES_DIR = "Z:\\! coding\\C++\\! builds\\"; // modify this based on where you put the files 
-    std::array<std::string, 4> options = {"Black-Jack-sim\\blackjack.exe","Poker-sim\\Poker-sim.exe","Roulette-simulator\\src\\Roulette.exe", "Texas_holdem-sim\\poker-sim.exe"};
+    std::array<std::string, 4> options = {"Black-Jack-sim\\blackjack.exe","Poker-sim\\Poker-sim.exe","Roulette-simulator\\roulette.exe", "Texas_holdem-sim\\poker-sim.exe"};
 
     bootingSequence();
     clearScreen();
